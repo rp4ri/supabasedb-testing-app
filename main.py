e6388f0d8e5bcae6019d4f0834e16fe08820e672
rp4ri <rodrigo.parisusao25201@gmail.com>
2024-01-12 14:34:06 -0400

    add create_item_endpoint to main.py and update requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from fastapi import FastAPI
-from supabase import create_client, Client
-import os
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from dotenv import load_dotenv
-load_dotenv()
+from settings import get_db
+from schemas import ItemCreate, Item
+from crud import create_item
 
 app = FastAPI()
 
-supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/items/", response_model=Item)
+def create_item_endpoint(item: ItemCreate, db: Session = Depends(get_db)):
+    return create_item(db=db, item=item)
